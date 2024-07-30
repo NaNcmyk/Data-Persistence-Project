62ce3868b505887d0db50cc6bf9c12893c28d124
nancy <dearrrnancy@gmail.com>
2024-07-30 18:45:22 -0400

    added best score ui & basic input field validation to menu scene

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
+using UnityEditorInternal;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -11,20 +12,60 @@ using UnityEditor;
 
 public class MenuUI : MonoBehaviour
 {
-    public TMP_Text playerName;
+    public Button startButton;
+    void Start()
+    {
+        startButton = GameObject.FindWithTag("StartButton").GetComponent<Button>();
+        startButton.interactable = false; // disable start button on start
+    }
 
-    public void StartGame()
+    // START BUTTON - on click function ------------------------------------------------
+    [SerializeField]
+    void StartGame()
     {
         SceneManager.LoadScene(1);
+        Debug.Log($"Current Player: {PlayerDataManager.Instance.playerName}");
     }
 
-    public void SetUserName()
+    // INPUT FIELD - on change function -----------------------------------------------------
+    // player must interact with--i.e., type something into--input field to enable start button
+    // name defaults to "Anonymous" if player inputs an empty string (e.g., white space)
+    [SerializeField]
+    void SetPlayerName()
     {
-        PlayerDataManager.Instance.playerName = playerName.text;
-        print(PlayerDataManager.Instance.playerName);
+        TMP_InputField inputField = GameObject.FindFirstObjectByType<TMP_InputField>();
+        string name = string.IsNullOrWhiteSpace(inputField.text) ? "Anonymous" : FormatName(inputField.text);
+        PlayerDataManager.Instance.playerName = name;
+
+        startButton.interactable = true;
     }
 
-    public void Quit()
+    string FormatName(string name)
+    {
+        string formattedName = "";
+
+        // replace non-alphanumeric chars with dashes
+        foreach (char letter in name)
+        {
+            if (!char.IsLetterOrDigit(letter))
+            {
+                formattedName += '-';
+            }
+            else
+            {
+                formattedName += letter;
+            }
+        }
+
+        //  truncate input if it exceeds 12 chars
+        formattedName = formattedName.Length > 12 ? formattedName[..12] : formattedName;
+
+        return formattedName;
+    }
+
+    // CLOSE ('x') BUTTON - on click function ----------------------------------------------
+    [SerializeField]
+    void Quit()
     {
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
--- Assets/Scripts/PlayerDataManager.cs
+++ Assets/Scripts/PlayerDataManager.cs
@@ -1,12 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class PlayerDataManager : MonoBehaviour
 {
    public static PlayerDataManager Instance;
 
+    // current player
     public string playerName;
+
+    // game data
+    public string bestPlayer;
+    public int bestScore;
 
     void Awake() {
         if (Instance != null) {
@@ -17,4 +24,11 @@ public class PlayerDataManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
+
+    [System.Serializable]
+    public class SaveData {
+        public string bestPlayer;
+        public int bestScore;
+    }
+
 }
